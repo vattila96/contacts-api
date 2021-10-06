035f78a5d32032c593e960ad067f098fdb06e338
vattila96 <srep.ati@gmail.com>
2021-10-06 15:38:08 +0200

    enable jpa auditing

--- src/main/java/hu/futureofmedia/task/contactsapi/ContactsApiApplication.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/ContactsApiApplication.java
@@ -2,8 +2,10 @@ package hu.futureofmedia.task.contactsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ContactsApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ContactsApiApplication.class, args);
--- src/main/java/hu/futureofmedia/task/contactsapi/entities/ContactPerson.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/entities/ContactPerson.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.*;
 import java.util.Date;
 import java.util.Optional;
@@ -12,6 +13,7 @@ import java.util.Optional;
 @Entity
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class ContactPerson {
 
     @Id
